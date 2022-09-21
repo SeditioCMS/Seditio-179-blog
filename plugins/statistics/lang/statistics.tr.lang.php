7d54075821662dc61b634ac33697238f4c583025
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:56:55 +0300

    Create statistics.tr.lang.php

---
+++ plugins/statistics/lang/statistics.tr.lang.php
@@ -0,0 +1,50 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+[BEGIN_SED]
+File=plugins/lang/statistics.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+
+$L['plu_title'] = "İstatistik";
+$L['plu_totalpagessince'] = "Tarihten beri görüntülenen toplam sayfa sayısı";
+$L['plu_maxwasreached'] = "Maksimum isabet sayısına ulaşıldı";
+$L['plu_pagesdisplayedthisday'] = "sayfalar bu gün görüntülendi.";
+$L['plu_totalmails'] = "Gönderilen postalar";
+$L['plu_totalpms'] = "Özel mesajlar gönderildi";
+$L['plu_totalactivepms'] = "Aktif mesajlar";
+$L['plu_totalarchivedpms'] = "Arşivlenmiş mesajlar";
+$L['plu_dbpages'] = "Sayfalar";
+$L['plu_dblinks'] = "Linkler";
+$L['plu_dbevents'] = "Etkinlikler";
+$L['plu_dbcomments'] = "Yorumlar";
+$L['plu_flag'] = "Flag";
+$L['plu_country'] = "Ülke";
+$L['plu_registeredusers'] = "Kayıtlı üyeler";
+$L['plu_membersbycountry'] = "Kayıtlı üyeler, ülkeye göre";
+$L['plu_pm'] = "Özel mesajlar";
+$L['plu_pollsratings'] = "Anketler ve derecelendirmeler";
+$L['plu_pfs'] = "Kişisel Dosya Alanı";
+$L['plu_pfsspace'] = "Tüm PFS'deki toplam dosya sayısı";
+$L['plu_pfssize'] = "PFS toplam boyutu";
+$L['plu_contributions'] = "Katkılarınız";
+$L['plu_notloggedin'] = "Mevcut değil, giriş yapmadınız.";
+$L['plu_comments'] = "Yorumlar";
+$L['plu_pagesrated'] = "Derecelendirilen sayfalar";
+$L['plu_votesratings'] = "Derecelendirmeler için oylar";
+$L['plu_polls'] = "Anketler";
+$L['plu_votespolls'] = "Anketler için oylar";
+$L['plu_usersfrom'] = "Bu ülkeden üyeler";
+$L['plu_unknown'] = "Bilinmiyor veya ayarlanmadı";
+$L['plu_total'] = "Toplam :";
+
+?>
