2aa79bb7832b225945518915cd81dc5767293272
Seditio CMS <bilgi@ntka.org>
2022-09-21 14:27:52 +0300

    Create adminqv.tr.lang.php

---
+++ plugins/adminqv/lang/adminqv.tr.lang.php
@@ -0,0 +1,41 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+
+[BEGIN_SED]
+File=plugins/adminqv/lang/adminqv.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=Plugin
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+if (!defined('SED_CODE')) { die('Wrong URL.'); }
+
+$L['plu_title'] = "Yöneticinin Hızlı Görünümü";
+
+$L['plu_pastdays'] = "Son 7 gündeki etkinlik";
+
+$L['plu_newusers'] = "Yeni üyeler";
+$L['plu_newpages'] = "Yeni sayfalar";
+$L['plu_newcomments'] = "Yeni yorumlar";
+$L['plu_newpms'] = "Yeni özel mesajlar";
+
+$L['plu_db'] = "SQL veritabanı İstatistik";
+
+$L['plu_db_rows'] = "SQL veritabanı, satır sayısı";
+$L['plu_db_indexsize'] = "SQL veritabanı, dizin boyutu (KB)";
+$L['plu_db_datassize'] = "SQL veritabanı, veri boyutu (KB)";
+$L['plu_db_totalsize'] = "SQL veritabanı, toplam boyut (KB)";
+$L['plu_db_fragmented'] = "SQL veritabanı, parçalanmış veri boyutu (KB)";
+
+$L['plu_hitsmonth'] = "Son 15 gündeki hit";
+
+
+
+?>
