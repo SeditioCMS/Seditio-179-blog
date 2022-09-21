3125ff8489f1650f678bf2286affd9204e5da251
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:22:55 +0300

    Create cleaner.tr.lang.php

---
+++ plugins/cleaner/lang/cleaner.tr.lang.php
@@ -0,0 +1,26 @@
+<?php
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+
+[BEGIN_SED]
+File=plugins/cleaner/lang/cleaner.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=Plugin
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['cfg_trashcan'] = array("* gün sonra çöp kutusunu boşalt (0 devre dışı).", "");
+$L['cfg_userprune'] = array("Etkinleştirilmemiş kullanıcıları * gün sonra sil (0 devre dışı).", "");
+$L['cfg_logprune'] = array("* gün sonra log kayıtlarını sil (0 devre dışı).", "");
+$L['cfg_refprune'] = array("* günden eski referer kayıtlarını sil (0 devre dışı).", "");
+$L['cfg_pmnotread'] = array("Alıcı tarafından okunmamış * günden eski özel mesajları sil (0 devre dışı).", "");
+$L['cfg_pmnotarchived'] = array("Arşivlenmemiş * günden eski özel mesajları sil (0 devre dışı).", "");
+$L['cfg_pmold'] = array("* günden eski TÜM özel mesajları sil (0 devre dışı).", "");
+
+?>
