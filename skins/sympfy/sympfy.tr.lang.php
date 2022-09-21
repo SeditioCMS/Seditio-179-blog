72a280a0a86b1b20253a99b82a6159598860249c
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:12:00 +0300

    Create sympfy.tr.lang.php

---
+++ skins/sympfy/sympfy.tr.lang.php
@@ -0,0 +1,295 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+[BEGIN_SED]
+File=skins/sympfy/sympfy.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=Skin
+Name=Sympfy
+Author=Amro
+Url=https://seditio.com.tr
+Description=Official skin
+[END_SED]
+==================== */
+
+//admin.tpl
+$skinlang['admin']['Help'] = "Yardım:";
+
+//comments.tpl
+$skinlang['comments']['Comment'] = "Yorum:";
+$skinlang['comments']['Send'] = "Gönder";
+$skinlang['comments']['Postedby'] = "Tarafından gönderildi";
+$skinlang['comments']['Commentedit'] = "Yorumu düzenle";   //New v173
+$skinlang['comments']['Update'] = "Güncelle";  //New v173
+$skinlang['comments']['Newcomment'] = "Yeni yorum";  //New v178
+
+//header.tpl
+$skinlang['header']['Login'] = "Giriş yapın";
+$skinlang['header']['Register'] = "Kayıt ol";
+$skinlang['header']['Lostyourpassword'] = "Şifreni mi unuttun?";
+
+//index.tpl
+$skinlang['index']['Recentadditions'] = "Ne var ne yok ?";
+$skinlang['index']['Polls'] = "Anketler";
+$skinlang['index']['Online'] = "Kim çevrimiçi ?";
+$skinlang['index']['Recentcomments'] = "Son Yorumlar";  // New in 175
+$skinlang['index']['Lastcomments'] = "Yorumlar"; // New in 178
+$skinlang['index']['Lastpages'] = "Sayfalar"; // New in 178
+$skinlang['index']['Activity'] = "Aktivite"; // New in 178
+
+//list.tpl
+$skinlang['list']['Page'] = "Sayfa";
+$skinlang['list']['linesperpage'] = "sayfa başına satır";
+$skinlang['list']['linesinthissection'] = "Bu bölümdeki çizgiler";
+$skinlang['list']['Comments'] = "Yorumlar";
+$skinlang['list']['Ratings'] = "Oylamalar";
+$skinlang['list']['hits'] = "hit";
+
+//news.tpl
+$skinlang['news']['by'] = "by";
+$skinlang['news']['title'] = "Haberler";
+
+//page.add.tpl
+$skinlang['pageadd']['Category'] = "Kategori:";
+$skinlang['pageadd']['Title'] = "Başlık:";
+$skinlang['pageadd']['Description'] = "Açıklama:";
+$skinlang['pageadd']['Author'] = "Yazar:";
+$skinlang['pageadd']['Extrakey'] = "Ekstra anahtar (isteğe bağlı):";
+$skinlang['pageadd']['Alias'] = "Alias:";
+$skinlang['pageadd']['Owner'] = "Sahip:";
+$skinlang['pageadd']['Begin'] = "Başlama:";
+$skinlang['pageadd']['Expire'] = "Bitiş:";
+$skinlang['pageadd']['Bodyofthepage'] = "Yazı alanı:";
+$skinlang['pageadd']['File'] = "Dosya indirme?";
+$skinlang['pageadd']['Filehint'] = "(Sayfanın alt kısmındaki indirme modülünü etkinleştirmek için 'evet'i seçin ve aşağıdaki 2 alanı doldurun)";
+$skinlang['pageadd']['URL'] = "URL:";
+$skinlang['pageadd']['URLhint'] = "(Dosya indir=hayır ise boş bırakın)";
+$skinlang['pageadd']['Filesize'] = "Filesize (KB):";
+$skinlang['pageadd']['Filesizehint'] = "(Dosya indir=hayır ise boş bırakın)";
+$skinlang['pageadd']['Formhint'] = "Gönderiminiz tamamlandıktan sonra, sayfa onay sırasına yerleştirilecek ve doğru bölümde görüntülenmeden önce bir site yöneticisi veya moderatörden onay beklecek..<br />Tüm alanları dikkatlice kontrol edin, daha sonra değiştiremezsiniz.<br />Gerçekten bir şeyi değiştirmeniz gerekiyorsa, bir moderatöre veya yöneticiye sorun..";
+$skinlang['pageadd']['Submit'] = "Gönder";
+$skinlang['pageadd']['Publish'] = "Yayınla"; // New v173
+$skinlang['pageadd']['Parsing'] = "Ayrıştırma:"; // New v173
+$skinlang['pageadd']['Thumbs'] = "Küçük resimler:"; // New v178
+
+$skinlang['pageadd']['Allowcomments'] = "Yorumlara izin ver?"; // New v173
+$skinlang['pageadd']['Allowratings'] = "Derecelendirmeye izin ver?"; // New v173
+
+//page.edit.tpl
+$skinlang['pageedit']['Category'] = "Kategori:";
+$skinlang['pageedit']['Title'] = "Başlık:";
+$skinlang['pageedit']['Description'] = "Açıklama:";
+$skinlang['pageedit']['Author'] = "Yazar:";
+$skinlang['pageedit']['Owner'] = "Sahip:";
+$skinlang['pageedit']['Date'] = "Tarih:";
+$skinlang['pageedit']['Begin'] = "Başlama:";
+$skinlang['pageedit']['Expire'] = "Bitiş:";
+$skinlang['pageedit']['Pagehitcount'] = "Sayfa hit sayısı:";
+$skinlang['pageedit']['Extrakey'] = "Ekstra anahtar";
+$skinlang['pageedit']['Alias'] = "Alias:";
+$skinlang['pageedit']['Parsing'] = "Ayrıştırma:";
+$skinlang['pageedit']['Bodyofthepage'] = "Yazı alanı:";
+$skinlang['pageedit']['Filedownload'] = "Dosya indirme?";
+$skinlang['pageedit']['URL'] = "URL:";
+$skinlang['pageedit']['URLhint'] = "(Dosya indirme=Evet ise)";
+$skinlang['pageedit']['Filesize'] = "Filesize (KB):";
+$skinlang['pageedit']['Filesizehint'] = "(Dosya indirme=Evet ise)";
+$skinlang['pageedit']['Filehitcount'] = "Dosya indirme sayısı:";
+$skinlang['pageedit']['Filehitcounthint'] = "(Dosya indirme=Evet ise)";
+$skinlang['pageedit']['Pageid'] = "Page ID:";
+$skinlang['pageedit']['Deletethispage'] = "!Bu sayfayı sil!:";
+$skinlang['pageedit']['Update'] = "Güncelle";
+$skinlang['pageedit']['Thumbs'] = "Küçük resimler:"; // New v178
+
+$skinlang['pageedit']['Allowcomments'] = "Yorumlara izin ver?"; // New v173
+$skinlang['pageedit']['Allowratings'] = "Derecelendirmeye izin ver?"; // New v173
+
+//page.tpl
+$skinlang['page']['Author'] = "Yazar:";
+$skinlang['page']['Submittedby'] = "Gönderen:";
+$skinlang['page']['Date'] = "Tarih:";
+$skinlang['page']['Comments'] = "Yorumlar:";
+$skinlang['page']['Ratings'] = "Oylamalar:";
+$skinlang['page']['Summary'] = "Özet:";
+$skinlang['page']['FilesizeKB'] = "KB";
+$skinlang['page']['Filesize'] = "Dosya boyutu:";
+$skinlang['page']['Membersrating'] = "Üye değerlendirmesi:";
+$skinlang['page']['Download'] = "Download:";
+$skinlang['page']['Hits'] = "Hit:";
+$skinlang['page']['Otherpages'] = "Diğer sayfalar";
+$skinlang['page']['Similarpages'] = "Benzer sayfalar";
+
+//pfs.tpl
+$skinlang['pfs']['Insertasthumbnail'] = "Küçük resim olarak ekle";
+$skinlang['pfs']['Insertasimage'] = "Tam boyutlu resim olarak ekle";
+$skinlang['pfs']['Insertaslink'] = "Dosyaya bağlantı olarak ekle";
+
+//pm.send.tpl
+$skinlang['pmsend']['Sendmessageto'] = "Mesajı gönder:";
+$skinlang['pmsend']['Sendmessagetohint'] = "(Virgülle ayırarak en fazla 10 alıcıya gönderin.)";
+$skinlang['pmsend']['Subject'] = "Konu:";
+$skinlang['pmsend']['Message'] = "Mesaj:";
+$skinlang['pmsend']['Sendmessage'] = "Mesajı şimdi gönder !";
+
+//pm.tpl
+$skinlang['pm']['State'] = "Durum";
+$skinlang['pm']['Sender'] = "Gönderen";
+$skinlang['pm']['SubjectClick'] = "Konu (okumak için tıklayın)";		// New in v125
+$skinlang['pm']['Date'] = "Tarih";
+$skinlang['pm']['Action'] = "Eylem";
+$skinlang['pm']['Recipient'] = "Alıcı";
+$skinlang['pm']['Nomessages'] = "Mesaj yok.";
+$skinlang['pm']['Page'] = "Sayfa";
+$skinlang['pm']['Subject'] = "Konu";
+$skinlang['pm']['Sender'] = "Gönderen";
+$skinlang['pm']['Recipient'] = "Alıcı";
+$skinlang['pm']['Date'] = "Tarih";
+$skinlang['pm']['Newmessage'] = "Yeni Mesaj";
+$skinlang['pm']['Message'] = "Mesaj";
+$skinlang['pm']['Reply'] = "Yanıt";
+$skinlang['pm']['Sendtoarchives'] = "Arşive gönder";
+$skinlang['pm']['Delete'] = "Sil";
+
+//polls.tpl
+$skinlang['polls']['voterssince'] = "voters since";
+$skinlang['polls']['Comments'] = "Yorumlar:";
+$skinlang['polls']['Allpolls'] = "Tüm anketler (mevcut ve arşivlenmiş)";
+
+//ratings.tpl
+$skinlang['ratings']['Averagemembersrating'] = "Ortalama üye puanı (10 üzerinden) :";
+$skinlang['ratings']['Votes'] = "oylar:";
+$skinlang['ratings']['Rate'] = "Oran:";
+$skinlang['ratings']['Rateit'] = "Oyla !";
+
+//user.auth.tpl
+$skinlang['usersauth']['Username'] = "Kullanıcı adı:";
+$skinlang['usersauth']['Password'] = "Şifre:";
+$skinlang['usersauth']['Rememberme'] = "Beni hatırla ?";
+$skinlang['usersauth']['Login'] = "Giriş";
+$skinlang['usersauth']['Register'] = "Kayıt olmak için buraya tıklayın";
+$skinlang['usersauth']['Lostpassword'] = "Şifreni mi unuttun ?";
+
+//users.details.tpl
+$skinlang['usersdetails']['Sendprivatemessage'] = "Özel mesaj gönder:";
+$skinlang['usersdetails']['Userfirstname'] = "İsim:";
+$skinlang['usersdetails']['Userlastname'] = "Soyisim:";
+$skinlang['usersdetails']['Maingroup'] = "Main grup:";
+$skinlang['usersdetails']['Memberof'] = "Üyesi:";
+$skinlang['usersdetails']['Country'] = "Ülke:";
+$skinlang['usersdetails']['Location'] = "şehir:";
+$skinlang['usersdetails']['Timezone'] = "Saat dilimi:";
+$skinlang['usersdetails']['Website'] = "Website:";
+$skinlang['usersdetails']['IRC'] = "IRC:";
+$skinlang['usersdetails']['ICQ'] = "ICQ:";
+$skinlang['usersdetails']['MSN'] = "MSN messenger:";
+$skinlang['usersdetails']['Birthdate'] = "Doğum tarihi:";
+$skinlang['usersdetails']['Age'] = "Yaş:";
+$skinlang['usersdetails']['Occupation'] = "Meslek:";
+$skinlang['usersdetails']['Gender'] = "Çinsiyet:";
+$skinlang['usersdetails']['Signature'] = "İmza:";
+$skinlang['usersdetails']['Registrationdate'] = "Kayıt Tarihi:";
+$skinlang['usersdetails']['Avatar'] = "Avatar:";
+$skinlang['usersdetails']['Photo'] = "Photo:";
+
+//users.edit.tpl
+$skinlang['usersedit']['UserID'] = "User ID:";
+$skinlang['usersedit']['Username'] = "Kullanıcı adı:";
+$skinlang['usersedit']['Userfirstname'] = "İsim:";
+$skinlang['usersedit']['Userlastname'] = "Soyisim:";
+$skinlang['usersedit']['Groupsmembership'] = "Grup üyeliği:";
+$skinlang['usersedit']['Maingroup'] = "Main grup:";
+$skinlang['usersedit']['Memberof'] = "Üyesi:";
+$skinlang['usersedit']['Country'] = "Ülke:";
+$skinlang['usersedit']['Location'] = "Şehir:";
+$skinlang['usersedit']['Timezone'] = "Saat dilimi:";
+$skinlang['usersedit']['Skin'] = "Tema:";
+$skinlang['usersedit']['Language'] = "Dil:";
+$skinlang['usersedit']['Avatar'] = "Avatar:";
+$skinlang['usersedit']['Signature'] = "İmza:";
+$skinlang['usersedit']['Photo'] = "Photo:";
+$skinlang['usersedit']['Newpassword'] = "Yeni bir şifre belirle:";
+$skinlang['usersedit']['Newpasswordhint'] = "(Geçerli parolayı<br />saklamak için boş bırakın)";
+$skinlang['usersedit']['Email'] = "Email:";
+$skinlang['usersedit']['Hidetheemail'] = "E-postayı her zaman gizle:";
+$skinlang['usersedit']['PMnotify'] = "PM bildir:";
+$skinlang['usersedit']['PMnotifyhint'] = "(Yeni özel mesaj olduğunda mail ile bildir)";
+$skinlang['usersedit']['Website'] = "Website:";
+$skinlang['usersedit']['IRC'] = "IRC:";
+$skinlang['usersedit']['ICQ'] = "ICQ:";
+$skinlang['usersedit']['MSN'] = "MSN messenger:";
+$skinlang['usersedit']['Birthdate'] = "Doğum tarihi:";
+$skinlang['usersedit']['Occupation'] = "Meslek:";
+$skinlang['usersedit']['Gender'] = "Cinsiyet:";
+$skinlang['usersedit']['Registeredsince'] = "Kayıt tarihi:";
+$skinlang['usersedit']['Lastlogged'] = "Son giriş:";
+$skinlang['usersedit']['LastIP'] = "Bilinen son IP:";
+$skinlang['usersedit']['Logcounter'] = "Günlük sayacı:";
+$skinlang['usersedit']['Deletethisuser'] = "! Bu kullanıcıyı sil !:";
+$skinlang['usersedit']['Update'] = "Güncelle";
+
+//users.profile.tpl
+$skinlang['usersprofile']['Username'] = "Kullanıcı adı:";
+$skinlang['usersprofile']['Userfirstname'] = "isim:";
+$skinlang['usersprofile']['Userlastname'] = "Soyisim:";
+$skinlang['usersprofile']['Groupsmembership'] = "Grup üyeliği:";
+$skinlang['usersprofile']['Maingroup'] = "Main grup:";
+$skinlang['usersprofile']['Registeredsince'] = "kayıt tarihi:";
+$skinlang['usersprofile']['Email'] = "Email:";
+$skinlang['usersprofile']['Hidetheemail'] = "E-postayı her zaman gizle:";
+$skinlang['usersprofile']['PMnotify'] = "PM bildirim:";
+$skinlang['usersprofile']['PMnotifyhint'] = "(Yeni özel mesaj olduğunda e-posta ile bildir)";
+$skinlang['usersprofile']['Skin'] = "Tema:";
+$skinlang['usersprofile']['Language'] = "Dil:";
+$skinlang['usersprofile']['Country'] = "Ülke:";
+$skinlang['usersprofile']['Location'] = "Şehir:";
+$skinlang['usersprofile']['Timezone'] = "Saat dilimi:";
+$skinlang['usersprofile']['Website'] = "Website:";
+$skinlang['usersprofile']['IRC'] = "IRC:";
+$skinlang['usersprofile']['ICQ'] = "ICQ:";
+$skinlang['usersprofile']['MSN'] = "MSN messenger:";
+$skinlang['usersprofile']['Birthdate'] = "Doğum tarihi:";
+$skinlang['usersprofile']['Occupation'] = "Meslek:";
+$skinlang['usersprofile']['Gender'] = "Cinsiyet:";
+$skinlang['usersprofile']['Avatar'] = "Avatar:";
+$skinlang['usersprofile']['Photo'] = "Photo:";
+$skinlang['usersprofile']['Signature'] = "İmza:";
+$skinlang['usersprofile']['Newpassword'] = "Yeni bir şifre belirleyin:";
+$skinlang['usersprofile']['Newpasswordhint'] = "(Geçerli parolayı<br />saklamak için boş bırakın)";
+$skinlang['usersprofile']['Update'] = "Güncelle";
+
+//users.register.tpl
+$skinlang['usersregister']['Username'] = "Kullanıcı adı:";
+$skinlang['usersregister']['Userfirstname'] = "isim:";
+$skinlang['usersregister']['Userlastname'] = "Soyisim:";
+$skinlang['usersregister']['Validemail'] = "Geçerli e-posta:";
+$skinlang['usersregister']['Validemailhint'] = "Kayıt işlemini tamamlamak için geçerli bir e-posta gereklidir !";
+$skinlang['usersregister']['Password'] = "Şifre:";
+$skinlang['usersregister']['Confirmpassword'] = "Tekrar şifre:";
+$skinlang['usersregister']['Country'] = "Ülke:";
+$skinlang['usersregister']['Formhint'] = "Giriş yaptıktan sonra, hakkınızda daha fazla bilgi ayarlamak için profilinize bakabilirsiniz..";
+$skinlang['usersregister']['Submit'] = "Gönder";
+
+//users.tpl
+$skinlang['users']['Page'] = "Sayfa";
+$skinlang['users']['usersperpage'] = "sayfa başına kullanıcı";
+$skinlang['users']['usersinthissection'] = "Bu bölümdeki kullanıcılar";
+
+//maintenance.tpl
+$skinlang['maintenance']['Username'] = "Kullanıcı adı:";
+$skinlang['maintenance']['Password'] = "Şifre:";
+$skinlang['maintenance']['Login'] = "Giriş";
+
+//admintooltip.js
+$skinlang['admintooltip']['pageadd'] = "Sayfa ekle";
+$skinlang['admintooltip']['pageedit'] = "Sayfayı düzenle";
+$skinlang['admintooltip']['pageeditcategory'] = "Kategoriyi düzenle";
+$skinlang['admintooltip']['pageeditoption'] = "Yapılandırma seçeneğini düzenle";
+
+//service.message.tpl
+$skinlang['message']['gohome'] = "Eve git";
+
+?>
