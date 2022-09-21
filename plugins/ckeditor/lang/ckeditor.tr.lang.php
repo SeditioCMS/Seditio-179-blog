a6afbfff3e579c203e462295da4ee7821609d1e3
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:18:42 +0300

    Create ckeditor.tr.lang.php

---
+++ plugins/ckeditor/lang/ckeditor.tr.lang.php
@@ -0,0 +1,37 @@
+<?php
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+
+[BEGIN_SED]
+File=plugins/ckeditor/lang/ckeditor.tr.lang.php
+Version=179
+Updated=2012-оct-19
+Type=Plugin
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['cfg_ckeditor_skin'] =  array("Varsayılan tema", "");
+$L['cfg_ckeditor_lang'] =  array("Düzenleyici arayüz dili", "");
+$L['cfg_ckeditor_color_toolbar'] =  array("Araç çubuğu rengi", "");
+$L['cfg_ckeditor_other_textarea'] =  array("Tüm metin alanları için düzenleyiciyi kullan", "");
+$L['cfg_ckeditor_other_toolbar'] =  array("Diğer metin alanları için araç çubuğu", "");
+$L['cfg_newpagetext'] =  array("Sayfa eklemek için araç çubuğu", "");
+$L['cfg_rpagetext'] =  array("Sayfa düzenleme için araç çubuğu", "");
+$L['cfg_newpmtext'] =  array("Özel mesajlar için araç çubuğu", "");
+$L['cfg_rusertext'] =  array("Kullanıcı imzası için araç çubuğu", "");
+$L['cfg_rtext'] =  array("Yorumlar için araç çubuğu", "");
+$L['cfg_newpagetext_height'] =  array("Sayfa ekleme için metin alanı yüksekliği", "");
+$L['cfg_rpagetext_height'] =  array("Sayfa düzenleme için metin alanı yüksekliği", "");
+$L['cfg_newpmtext_height'] =  array("Özel mesajlar için metin alanı yüksekliği", "");
+$L['cfg_rusertext_height'] =  array("Kullanıcının imzası için metin alanının yüksekliği", "");
+$L['cfg_rtext_height'] =  array("Yorumlar metin kutusu yüksekliği", "");
+$L['cfg_ckeditor_other_height'] =  array("Diğer metin alanları için yükseklik", "");
+$L['cfg_ckeditor_global_toolbar'] =  array("Grup için genel araç çubuğu ", "");
+$L['cfg_ckeditor_detectlang'] =  array("Kullanıcı profilinden düzenleyici arayüz dilini belirleyin", "");
+
+?>
