ec8b9e90fb2b46e4466f8e6961b95cd2e7a06b49
Seditio CMS <bilgi@ntka.org>
2022-09-21 16:27:17 +0300

    Create syntaxhighlight.tr.lang.php

---
+++ plugins/syntaxhighlight/lang/syntaxhighlight.tr.lang.php
@@ -0,0 +1,21 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+
+[BEGIN_SED]
+File=plugins/syntaxhighlight//lang/syntaxhighlight.tr.lang.php
+Version=179
+Updated=2012-feb-16
+Type=Plugin
+Author=Amro
+Description=
+[END_SED]
+
+==================== */
+
+$L['cfg_syntaxhighlight_theme'] = array("Syntaxhighlight renk teması", "");
+
+?>
