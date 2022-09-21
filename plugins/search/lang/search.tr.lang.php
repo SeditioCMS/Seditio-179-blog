c28f9f33d5103c6695e06f87352a79cfa962bdd1
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:42:09 +0300

    Create search.tr.lang.php

---
+++ plugins/search/lang/search.tr.lang.php
@@ -0,0 +1,29 @@
+<?PHP
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+-----------------------
+[BEGIN_SED]
+File=plugins/search/lang/search.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['plu_title'] = "Arama";
+
+$L['plu_searchin'] = "<strong>Anahtar kelime(ler)</strong>";
+
+$L['plu_querytooshort'] = "Sorgu dizesi çok kısa !";
+$L['plu_toomanywords'] = "Çok fazla kelime girdiniz";
+$L['plu_found'] = "Bulundu";
+$L['plu_match'] = "eşleşen";
+$L['plu_nofound'] = "Eşleşme bulunamadı";
+$L['plu_allcategories'] = "Tüm Kategoriler";
+
+
+?>
