ba9f4708e0cb0abefe3ad933b89cdbf73501fac5
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:37:55 +0300

    Create passrecover.tr.lang.php

---
+++ plugins/passrecover/lang/passrecover.tr.lang.php
@@ -0,0 +1,36 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+[BEGIN_SED]
+File=plugins/passrecover/lang/passrecover.tr.lang.php
+Version=179
+Updated=2006-jun-05
+Type=Plugin.standalone
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+
+$L['plu_title'] = "Şifre kurtarma";
+
+$L['plu_explain1'] = "1 : E-posta adresinizi girin, şifrenizi nasıl sıfırlayacağınızla ilgili talimatları size e-posta ile gönderelim..";
+$L['plu_explain2'] = "2 : Acil durum bağlantısı içeren bir mesaj alacaksınız, oturum açmak için tıklayın.";
+$L['plu_explain3'] = "3 : Ardından profilinize gidin ve kendinize yeni bir şifre belirleyin.";
+$L['plu_explain4'] = "Profilinizdeki e-posta alanı boş ise, şifrenizi kurtaramazsınız..<br />Bu durumda, lütfen iletişim formu ile web yöneticisi ile iletişime geçin..";
+$L['plu_mailsent'] = "Bitti, lütfen birkaç dakika içinde posta kutunuzu kontrol edin ve acil durum bağlantısını tıklayın.<br />Ardından talimatları izleyin.";
+$L['plu_mailsent2'] = "Bitti, lütfen birkaç dakika içinde posta kutunuzu kontrol edin ve şifrenizi değiştirmek için bağlantıya tıklayın.. <br /> Ardından talimatları izleyin.";
+$L['plu_youremail'] = "E-posta adresiniz : ";
+$L['plu_request'] = "Şifre iste";
+$L['plu_loggedin1'] = "Tekrar hoşgeldiniz, ";
+$L['plu_loggedin2'] = "şimdi giriş yaptın.";
+$L['plu_loggedin3'] = "Şimdi Profiline gidebilirsin <a href=\"".sed_url("users", "m=profile")."\">profil</a>, ve kendinize yeni bir şifre belirleyin.";
+$L['plu_email1'] = "Bu e-postayı, siz (veya sizin gibi davranan biri) Seditio CMS tarafından desteklenen bir sitede oturum açmak için bir acil durum bağlantısı talep ettiğiniz için alıyorsunuz.. Bu e-postayı siz istemediyseniz lütfen dikkate almayın, almaya devam ederseniz lütfen site yöneticisiyle iletişime geçin..\n\nŞimdi aşağıdaki bağlantı ile giriş yapabilir, ardından talimatları takip edebilirsiniz. :";
+$L['plu_email2'] = "Web sitemize şifre kurtarmayı girmeniz isteniyor. \r\nYeni bir şifre oluşturmak için aşağıdaki bağlantıya tıklayın. E-posta adresinize yeni bir şifre gönderilecektir.";
+$L['plu_email3'] = "Talebinizde yeni bir şifre. En kısa sürede değiştirin ve bu e-postayı silin.\r\n\r\nYeni şifreniz: "; 
+$L['plu_newpass'] = "Tamam!<br /><br />Yakında siteye erişmek için yeni bir şifre alacaksınız.";
+
+?>
