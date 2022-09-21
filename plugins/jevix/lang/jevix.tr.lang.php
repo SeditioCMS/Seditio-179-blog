2a28452b4dd1274dd0ed2d417130c1f22e2464e2
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:27:57 +0300

    Create jevix.tr.lang.php

---
+++ plugins/jevix/lang/jevix.tr.lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+
+[BEGIN_SED]
+File=plugins/jevix/lang/jevix.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=Plugin
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['cfg_use_xhtml'] = array("Kullanmak XHTML ?", "");
+$L['cfg_use_for_admin'] = array("Yöneticiler için Jevix'i Etkinleştir", "");
+
+?>
