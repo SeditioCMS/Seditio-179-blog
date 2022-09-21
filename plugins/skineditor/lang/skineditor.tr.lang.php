674a1b048878dc035b070628b94383d3511e64d7
Seditio CMS <bilgi@ntka.org>
2022-09-21 15:51:01 +0300

    Create skineditor.tr.lang.php

---
+++ plugins/skineditor/lang/skineditor.tr.lang.php
@@ -0,0 +1,23 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+[BEGIN_SED]
+File=plugins/lang/skineditor.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+
+$L['plu_makbak'] = "Şimdi yedekle";
+$L['plu_delbak'] = "Yedeklemeyi sil";
+$L['plu_resbak'] = "Yedeklemeyi geri yükle";
+$L['plu_reopen'] = "Güncelle ve buraya geri dön";
+
+?>
