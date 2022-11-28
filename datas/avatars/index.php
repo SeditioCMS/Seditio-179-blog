4deb0d878a7a411cba419e34c07bf60c65f45e89
Seditio CMS <bilgi@ntka.org>
2022-11-28 13:18:28 +0300

    update

---
+++ datas/avatars/index.php
@@ -0,0 +1,20 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+http://www.neocrome.net
+http://www.seditiocms.com
+[BEGIN_SED]
+File=datas/avatars/index.php
+Version=177
+Updated=2015-feb-06
+Type=Core
+Author=Neocrome
+Description=Directory blocker
+[END_SED]
+==================== */
+
+/* ==== Directory blocker ==== */
+
+?>
---
+++ datas/resized/index.php
@@ -0,0 +1,19 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.org
+[BEGIN_SED]
+File=datas/resized/index.php
+Version=179
+Updated=2021-feb-06
+Type=Core
+Author=Seditio Team
+Description=Directory blocker
+[END_SED]
+==================== */
+
+/* ==== Directory blocker ==== */
+
+?>
