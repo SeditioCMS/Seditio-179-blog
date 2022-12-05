6f087262018ed43dc7cc1417289047343ea248f7
Seditio CMS <bilgi@ntka.org>
2022-12-05 15:59:44 +0300

    Update sympfy.php

--- skins/sympfy/sympfy.php
+++ skins/sympfy/sympfy.php
@@ -52,13 +52,6 @@ $cfg['plu_mask_pages'] = "<span class=\"rec-date\">%3\$s</span>"." "."%1\$s"." "
 // %2\$s = Link to the page
 // %3\$s = Date
 
-$cfg['plu_mask_topics'] =  "<span class=\"rec-date\">%2\$s</span>"." "."%3\$s"." ".$cfg['separator']." "."%4\$s"." ("."%5\$s".")<br />";
-// %1\$s = "Follow" image
-// %2\$s = Date
-// %3\$s = Section
-// %4\$s = Topic title
-// %5\$s = Number of replies
-
 $cfg['plu_mask_comments'] = "<span class=\"rec-date\">%3\$s</span> %1\$s"." ".$cfg['separator']." "."<span class=\"rec-comment\">%5\$s</span>"." ".$cfg['separator']." %2\$s<br />";
 // %1\$s = Link to the comment
 // %2\$s = Author
