754ab889d0e24fcf5d7a4ad9f3ba671322337a14
Seditio CMS <bilgi@ntka.org>
2023-12-26 12:33:57 +0300

    Update common.php

--- system/common.php
+++ system/common.php
@@ -443,7 +443,7 @@ $yesno_revers_arr = array(0 => $L['Yes'], 1 => $L['No']);
 /* ======== Who's online part 2 ======== */
 
 $out['whosonline'] = ($cfg['disablewhosonline']) ? '' : $sys['whosonline_reg_count'] . ' ' . $L['com_members'] . ', ' . $sys['whosonline_vis_count'] . ' ' . $L['com_guests'];
-$out['copyright'] = "<a href=\"https://seditio.org\">" . $L['foo_poweredby'] . " Seditio</a>";
+$out['copyright'] = "<a href=\"https://seditio.com.tr\">" . $L['foo_poweredby'] . " Seditio</a>";
 
 /* ======== Various ======== */
 
